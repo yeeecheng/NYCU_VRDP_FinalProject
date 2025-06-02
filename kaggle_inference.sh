3ad51b835f38a7a00e1982ed209cd5bec2e02c53
yeeecheng <yichengliao1003@gmail.com>
2025-06-02 23:24:27 +0800

    add READMD.md

--- kaggle_inference.sh
+++ kaggle_inference.sh
@@ -18,8 +18,7 @@
 
 CUDA_VISIBLE_DEVICES="5" python inference_with_DACLIP.py --input ./dataset/test/lr \
     --output ./kaggle_test_HR_result/pre-trained_with_kaggle_dataset_with_DACLIP_modify_TTA_v2_145000 \
-    --model_path /swim-pool/yicheng/NYCU_VRDP_FinalProject/experiments/train_DRCT_SRx4_finetune_from_DRCT_pre-train_with_DACLIP_modify_v2/models/net_g_25000.pth \
-    --start_idx 0 --end_idx 3600
+    --model_path /swim-pool/yicheng/NYCU_VRDP_FinalProject/experiments/train_DRCT_SRx4_finetune_from_DRCT_pre-train_with_DACLIP_modify_v2/models/net_g_25000.pth
 
 
 # CUDA_VISIBLE_DEVICES="0" python inference_with_DACLIP.py --input ./dataset/test/lr \
